4d4b3c3231f539f8c8e638c04054f742dcf4de12
akshay5606 <55668496+akshay5606@users.noreply.github.com>
2025-07-08 18:24:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,15 @@ dotenv.config(); // ✅ Load .env file first
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
- 
+ if(process.env.NODE_ENV==='production')
+ {
 // Serve static files
 app.use(express.static(path.join(__dirname, '../frontend/vite-project/dist')));
 // Fallback to index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/vite-project/dist/index.html'));
 });
- 
+}
 //const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => {
  // console.log(`Server running on port ${PORT}`);
